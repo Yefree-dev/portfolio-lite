a646e493601991bfbed3f53dc54d8ace5efce115
Yefree-dev <yefree.web.developer@gmail.com>
2025-04-28 12:20:32 -0400

    add UI crafts

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,34 +1,67 @@
-const dataUrl = "../data/projects.json";
-const projectsContainer = document.getElementById("project-container");
+// Projects #1
 
-fetch(dataUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    projectsContainer.innerHTML = data.projects
-      .map((item) => projectComponent(item))
-      .join("");
-    console.log(data.posts);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
+const buttonsContainer = document.getElementById("buttons-container");
+const buttonsSocialLinks = document.querySelectorAll(".main__btn");
+
+buttonsSocialLinks.forEach((button) => {
+  button.addEventListener("click", () => {
+    moveAfterIndicator(button);
+    changeTabs(button);
+  });
+});
+
+// Move ::after indicator hover effect on buttons
+
+function moveAfterIndicator(newTab) {
+  const newButtonWidth = newTab.offsetWidth / buttonsContainer.offsetWidth;
+
+  buttonsContainer.style.setProperty("--_left", newTab.offsetLeft + "px");
+  buttonsContainer.style.setProperty("--_width", newButtonWidth);
+  buttonsContainer
+    .querySelectorAll("button")
+    .forEach((btn) => btn.style.setProperty("--_color", "hsl(0, 0%, 25%)"));
+  newTab.style.setProperty("--_color", "hsl(0, 0%, 95%)");
+}
+
+function adaptHeight(currentTab) {
+  const panelContainer = document.querySelector(".main__panel");
+
+  const height = currentTab.offsetHeight;
+  console.log(height);
+
+  panelContainer.style.setProperty("--_height", height + 27 + "px");
+}
+
+function changeTabs(currentTab) {
+  // When user clicks the tab button obtain the aria-controls
+  const ariaControls = currentTab.getAttribute("aria-controls");
+
+  // set every button that is not the selected one hidden.
+  buttonsSocialLinks.forEach((button) => {
+    button.setAttribute("aria-selected", "false");
   });
 
-function projectComponent(item) {
-  return `
-      <li class="post__section__li">
-        <a href="${item.link}">
-          <div>
-            <span class="post__section__li-title">${item.title}</span>
-            <span>${item.description}</span>
-          </div>
-          <span class="post__section__li-line"></span>
-          <span>${item.date}</span>
-        </a>
-      </li>
-    `;
+  // except the button that's been clicked.
+  currentTab.setAttribute("aria-selected", "true");
+
+  // Make sure the button has the `aria-selected` property.
+  if (ariaControls === null) return;
+
+  console.log(ariaControls);
+
+  const allTabs = document.querySelectorAll(".main__panels");
+
+  allTabs.forEach((tab) => {
+    tab.setAttribute("aria-hidden", "true");
+  });
+  // Set the current tab aria-hidden="false"
+  const currentTabSelected = document.querySelector(
+    `.main__panels[data-panel="${ariaControls}"]`
+  );
+
+  currentTabSelected.setAttribute("aria-hidden", "false");
+  adaptHeight(currentTabSelected);
+
+  console.log(currentTabSelected);
+  // .setAttribute("aria-hidden", "true");
 }
